5933d0f3432de9956fb0830541b30bde36a6ca34
Eduardo <eoalcantara@gmail.com>
2023-06-20 14:58:58 -0300

    data json included

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Container from './components/Container';
+import data from './products.json';
 
 const Item = ({ item }) => {
   return (
     <li className='item' data-testid='product-item'>
       { /* TODO */ }
       <img src="thumbnail here"></img>
-      <h4> title here </h4>
-      <p> description here </p>
+      <h4> {data[0].id}  </h4>
+      <p>  1 description here </p>
       <span> brand here </span>
       <span> price here </span>
     </li>
